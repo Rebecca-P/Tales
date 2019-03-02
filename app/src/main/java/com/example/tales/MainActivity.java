cf020ac915322992b62fb0e59e11501e57175193
Rebecca PORCHETTE <Rebecca.Porchette@uqtr.ca>
2019-03-02 23:09:08 +0100

    Initial commit

---
+++ app/src/main/java/com/example/tales/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.tales;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    //private Button mPlayButton ;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void sendtoMe2 (View view)
+    {
+//direction activity_new_one
+        Intent nameIntent = new Intent(this, Menu.class);
+        startActivity(nameIntent);
+    }
+
+
+}
---
+++ app/src/main/java/com/example/tales/Menu.java
@@ -0,0 +1,18 @@
+package com.example.tales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.view.View;
+import android.content.Intent;
+
+
+public class Menu extends AppCompatActivity{
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu);
+    }
+}
